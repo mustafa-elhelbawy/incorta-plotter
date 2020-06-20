2c802addd7c421ff9815c2566469cec1ab4fcbbe
Mustafa <mustafa@mustafa.elhelbawy>
2020-06-20 13:54:13 +0200

    prepare drag & drop API areas, state to display selected items

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
-import columns from './columns';
 import loader from './loader';
+import columns from './columns';
+import dimension from './dimension';
+import measure from './measure';
 
 export default combineReducers({
+    loader,
     columns,
-    loader
+    dimension,
+    measure
 });
